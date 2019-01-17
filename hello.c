163e7f4d169ecfd10ab320bdfdebc9bfa75c34ed
tsmith403 <46788808+tsmith403@users.noreply.github.com>
2019-01-17 09:58:38 -0600

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Trevor Smith
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Trevor Smith Andrew Fujan\n");
+  printf("Anthropology Finance\n");
+
+  return 0;
+}
